c8906eeedde14245a39b0e9b3de9846395d4b2e8
atitus5 <drewtitus59@gmail.com>
2017-05-10 22:30:10 -0400

    Various other posterior analysis code; no major improvements

--- data_analysis/posteriors.py
+++ data_analysis/posteriors.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.signal import butter, lfilter
+from scipy.stats import mode
 import sys
 
 if len(sys.argv) != 2:
@@ -44,7 +46,7 @@ for i in xrange(total_utterances):
     classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
     if classification == label:
         correct += 1
-print "Classified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+print "\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
 
 print "Classifying based on max probabilities..."
 correct = 0
@@ -54,14 +56,52 @@ for i in xrange(total_utterances):
     classification = (np.argmax(log_posteriors_matrix) % 4) + 1     # The classifier used 1-indexing...
     if classification == label:
         correct += 1
-print "Classified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+print "\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+print "Classifying based on probability modes..."
+correct = 0
+for i in xrange(total_utterances):
+    label = log_posteriors[i][1]
+    log_posteriors_matrix = np.asarray(log_posteriors[i][2])
+    num_posteriors = log_posteriors_matrix.shape[0]
+    frame_classifications = [np.argmax(log_posteriors_matrix[i, :]) for i in xrange(num_posteriors)]
+    classification = mode(frame_classifications)[0] + 1     # The classifier used 1-indexing...
+    if classification == label:
+        correct += 1
+print "\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+print "Classifying based on longest run..."
+correct = 0
+for i in xrange(total_utterances):
+    label = log_posteriors[i][1]
+    log_posteriors_matrix = np.asarray(log_posteriors[i][2])
+    num_posteriors = log_posteriors_matrix.shape[0]
+
+    longest_run = 0
+    longest_classification = -1     # Forces us to evaluate first element
+    current_run = 0
+    current_classification = -1     # Forces us to evaluate first element
+    for j in xrange(num_posteriors):
+        frame_classification = np.argmax(log_posteriors_matrix[j, :])
+        if frame_classification != current_classification:
+            current_classification = frame_classification
+            current_run = 0     # Reset --- it will be incremented below
+
+        current_run += 1
+        if current_run > longest_run:
+            longest_classification = current_classification
+            longest_run += 1
+
+    classification = longest_classification + 1     # The classifier used 1-indexing...
+    if classification == label:
+        correct += 1
+print "\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
 
 lpf_order = 3
 lpf_normfreq = 0.05
 lpf_b, lpf_a = butter(lpf_order, lpf_normfreq)
 print "Classifying based on average of low-pass filtered posteriors..."
 correct = 0
-
 for i in xrange(total_utterances):
     label = log_posteriors[i][1]
     log_posteriors_matrix = np.asarray(log_posteriors[i][2])
@@ -72,38 +112,199 @@ for i in xrange(total_utterances):
     classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
     if classification == label:
         correct += 1
-print "Classified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+print "\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
 
-'''
-f, axes = plt.subplots(4, 1)
-axes[0].set_ylabel("Log Posterior Probability")
-axes[1].set_ylabel("Log Posterior Probability")
-axes[2].set_ylabel("Log Posterior Probability")
-axes[3].set_ylabel("Log Posterior Probability")
 
-unfiltered = axes[0].plot(range(num_posteriors), log_posteriors_matrix[:, 0], label="Out-of-Set (u)")
-filtered = axes[0].plot(range(num_posteriors), filtered_posteriors_matrix[:, 0], label="Out-of-Set (f)")
-lines = unfiltered + filtered
-labels = [line.get_label() for line in lines]
-axes[0].legend(lines, labels)
 
-unfiltered = axes[1].plot(range(num_posteriors), log_posteriors_matrix[:, 1], label="English (u)")
-filtered = axes[1].plot(range(num_posteriors), filtered_posteriors_matrix[:, 1], label="English (f)")
-lines = unfiltered + filtered
-labels = [line.get_label() for line in lines]
-axes[1].legend(lines, labels)
+# Normal moving average
 
-unfiltered = axes[2].plot(range(num_posteriors), log_posteriors_matrix[:, 2], label="German (u)")
-filtered = axes[2].plot(range(num_posteriors), filtered_posteriors_matrix[:, 2], label="German (f)")
-lines = unfiltered + filtered
-labels = [line.get_label() for line in lines]
-axes[2].legend(lines, labels)
+for window in xrange(1, 11):
+    print "Moving average (window = %d)..." % window
+    smoothed_posteriors = []
+    for i in xrange(total_utterances):
+        log_posteriors_matrix = np.asarray(log_posteriors[i][2])
+        num_classes = log_posteriors_matrix.shape[1]
+        smoothed = []
+        for i in xrange(num_classes):
+            # Avoid the NaN at the beginning
+            smoothed.append(pd.Series(log_posteriors_matrix[:, i]).rolling(window=window).mean()[window - 1:])
+        smoothed = np.asarray(smoothed).T
+        smoothed_posteriors.append(smoothed)
+    smoothed_posteriors = np.asarray(smoothed_posteriors)
 
-unfiltered = axes[3].plot(range(num_posteriors), log_posteriors_matrix[:, 3], label="Mandarin (u)")
-filtered = axes[3].plot(range(num_posteriors), filtered_posteriors_matrix[:, 3], label="Mandarin (f)")
-lines = unfiltered + filtered
-labels = [line.get_label() for line in lines]
-axes[3].legend(lines, labels)
+    print "\tClassifying based on average probabilities..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+        avg_posteriors = np.divide(np.sum(smoothed_posteriors_matrix, axis=0),
+                                   np.ones(smoothed_posteriors_matrix.shape[1]) * num_posteriors)
+        classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
 
-plt.show()
-'''
+    print "\tClassifying based on max probabilities..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        classification = (np.argmax(smoothed_posteriors_matrix) % 4) + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    print "\tClassifying based on probability modes..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+        frame_classifications = [np.argmax(smoothed_posteriors_matrix[i, :]) for i in xrange(num_posteriors)]
+        classification = mode(frame_classifications)[0] + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    print "\tClassifying based on longest run..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+
+        longest_run = 0
+        longest_classification = -1     # Forces us to evaluate first element
+        current_run = 0
+        current_classification = -1     # Forces us to evaluate first element
+        for j in xrange(num_posteriors):
+            frame_classification = np.argmax(smoothed_posteriors_matrix[j, :])
+            if frame_classification != current_classification:
+                current_classification = frame_classification
+                current_run = 0     # Reset --- it will be incremented below
+
+            current_run += 1
+            if current_run > longest_run:
+                longest_classification = current_classification
+                longest_run += 1
+
+        classification = longest_classification + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    lpf_order = 3
+    lpf_normfreq = 0.05
+    lpf_b, lpf_a = butter(lpf_order, lpf_normfreq)
+    print "\tClassifying based on average of low-pass filtered posteriors..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        filtered_posteriors_matrix = np.asarray([lfilter(lpf_b, lpf_a, smoothed_posteriors_matrix[:, j]) for j in xrange(smoothed_posteriors_matrix.shape[1])]).T
+        num_posteriors = filtered_posteriors_matrix.shape[0]
+        avg_posteriors = np.divide(np.sum(filtered_posteriors_matrix, axis=0),
+                                   np.ones(filtered_posteriors_matrix.shape[1]) * num_posteriors)
+        classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+
+
+# Exponentially weighted moving average
+
+for window in xrange(1, 11):
+    print "Exponentially weight moving average (window = %d)..." % window
+    smoothed_posteriors = []
+    for i in xrange(total_utterances):
+        log_posteriors_matrix = np.asarray(log_posteriors[i][2])
+        num_classes = log_posteriors_matrix.shape[1]
+        smoothed = []
+        for i in xrange(num_classes):
+            # Avoid the NaN at the beginning
+            smoothed.append(pd.Series(log_posteriors_matrix[:, i]).ewm(span=window).mean()[window - 1:])
+        smoothed = np.asarray(smoothed).T
+        smoothed_posteriors.append(smoothed)
+    smoothed_posteriors = np.asarray(smoothed_posteriors)
+
+    print "\tClassifying based on average probabilities..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+        avg_posteriors = np.divide(np.sum(smoothed_posteriors_matrix, axis=0),
+                                   np.ones(smoothed_posteriors_matrix.shape[1]) * num_posteriors)
+        classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    print "\tClassifying based on max probabilities..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        classification = (np.argmax(smoothed_posteriors_matrix) % 4) + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    print "\tClassifying based on probability modes..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+        frame_classifications = [np.argmax(smoothed_posteriors_matrix[i, :]) for i in xrange(num_posteriors)]
+        classification = mode(frame_classifications)[0] + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    print "\tClassifying based on longest run..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        num_posteriors = smoothed_posteriors_matrix.shape[0]
+
+        longest_run = 0
+        longest_classification = -1     # Forces us to evaluate first element
+        current_run = 0
+        current_classification = -1     # Forces us to evaluate first element
+        for j in xrange(num_posteriors):
+            frame_classification = np.argmax(smoothed_posteriors_matrix[j, :])
+            if frame_classification != current_classification:
+                current_classification = frame_classification
+                current_run = 0     # Reset --- it will be incremented below
+
+            current_run += 1
+            if current_run > longest_run:
+                longest_classification = current_classification
+                longest_run += 1
+
+        classification = longest_classification + 1     # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
+    lpf_order = 3
+    lpf_normfreq = 0.05
+    lpf_b, lpf_a = butter(lpf_order, lpf_normfreq)
+    print "\tClassifying based on average of low-pass filtered posteriors..."
+    correct = 0
+    for i in xrange(total_utterances):
+        label = log_posteriors[i][1]
+        smoothed_posteriors_matrix = np.asarray(smoothed_posteriors[i])
+        filtered_posteriors_matrix = np.asarray([lfilter(lpf_b, lpf_a, smoothed_posteriors_matrix[:, j]) for j in xrange(smoothed_posteriors_matrix.shape[1])]).T
+        num_posteriors = filtered_posteriors_matrix.shape[0]
+        avg_posteriors = np.divide(np.sum(filtered_posteriors_matrix, axis=0),
+                                   np.ones(filtered_posteriors_matrix.shape[1]) * num_posteriors)
+        classification = np.argmax(avg_posteriors) + 1      # The classifier used 1-indexing...
+        if classification == label:
+            correct += 1
+    print "\t\tClassified with UER %.3f" % (1.0 - (correct / float(total_utterances)))
+
